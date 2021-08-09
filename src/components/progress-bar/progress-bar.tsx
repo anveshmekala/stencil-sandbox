121259d323ea0ea0de8b99beba2cc738a2b697f0
anveshmekala <anv11827@esri.com>
2021-08-09 17:42:41 -0500

    remove <progress-bar> html element

--- src/components/progress-bar/progress-bar.tsx
+++ src/components/progress-bar/progress-bar.tsx
@@ -6,12 +6,8 @@ import { Component, h, Host, Listen, Prop } from '@stencil/core';
   shadow: true,
 })
 export class ProgressBar {
-  @Prop({ reflect: true, mutable: true }) current: number;
+  @Prop({ reflect: true, mutable: true }) current: number =0;
   @Prop() type: string
-  @Listen('click', { capture: true })
-  handleClick() {
-    this.current = 100
-  }
 
   render() {
     const progressbarstyle = { width: `${this.current}%` }
@@ -21,19 +17,8 @@ export class ProgressBar {
           <h2>
             Progress Bar
           </h2>
-          {/* <progress class="infinite" id="file" value={this.current} max="100" style=> </progress> */}
-          <div class="pot">
-
-
-          {/* <div class="infinite" style={progressbarstyle
-          }></div> */}
-          <div class="infinite">
-
+          <div class="infinite" style={progressbarstyle}>
           </div>
-                    </div>
-          {/* <button >
-            animate
-          </button> */}
         </div>
       </Host>
     );
