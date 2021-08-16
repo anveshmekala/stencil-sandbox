449c87817b55555fc1fd0954abb9ac018e021f4f
anveshmekala <anv11827@esri.com>
2021-08-16 12:21:43 -0500

    add E2E tests

--- src/components/toggle-button/test/toggle-button.e2e.ts
+++ src/components/toggle-button/test/toggle-button.e2e.ts
@@ -8,4 +8,15 @@ describe('toggle-button', () => {
     const element = await page.find('toggle-button');
     expect(element).toHaveClass('hydrated');
   });
+  
+  it('renders', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<toggle-button></toggle-button>');
+
+    const element = await page.find('toggle-button >>> input');
+    await  element.click()
+    const text = await page.find('toggle-button >>> h4')
+    expect(text).toEqualText('ON')
+  });
+
 });
